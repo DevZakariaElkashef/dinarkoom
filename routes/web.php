13a54da062345896a761fc3a95920eb4dd44f5f6
dev.zakariaelkashef <dev.zakariaelkashef@gmail.com>
2023-10-04 00:00:09 +0300

    add lagn fils

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\AboutUsController;
 use App\Http\Controllers\Admin\BannerController;
 use App\Http\Controllers\Admin\ContactController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
@@ -42,100 +41,82 @@ Route::group(
         'prefix' => LaravelLocalization::setLocale(),
         'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
     ],
-function () {
-        
-    Auth::routes();
-   
-    Route::get('/email/verify', function () {
-        return view('site.auth.verify');
-    })->middleware('auth')->name('verification.notice');
+    function () {
 
-    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-        $request->fulfill();
-     
-        return redirect('/');
-    })->middleware(['auth', 'signed'])->name('verification.verify');
+        Auth::routes();
 
-    Route::post('/email/verification-notification', function (Request $request) {
-        $request->user()->sendEmailVerificationNotification();
-     
-        return back()->with('message', 'Verification link sent!');
-    })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+        Route::get('/email/verify', function () {
+            return view('site.auth.verify');
+        })->middleware('auth')->name('verification.notice');
 
+        Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+            $request->fulfill();
 
-    Route::get('/', [HomeController::class, 'index'])->name('home');
+            return redirect('/');
+        })->middleware(['auth', 'signed'])->name('verification.verify');
 
-    Route::get('guest/register', [GuestController::class, 'register'])->name('guest.register');
-    Route::post('guest/register', [GuestController::class, 'store'])->name('guest.store');
+        Route::post('/email/verification-notification', function (Request $request) {
+            $request->user()->sendEmailVerificationNotification();
+
+            return back()->with('message', 'Verification link sent!');
+        })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 
+        Route::get('/', [HomeController::class, 'index'])->name('home');
 
-
-    Route::middleware(['auth', 'verified'])->group(function(){
-
-        Route::get('profile', [ProfileController::class, 'index'])->name('profile.index');
-        Route::post('profile/update', [ProfileController::class, 'update'])->name('profile.update');
-        Route::get('profile/edit-password', [ProfileController::class, 'editPassword'])->name('profile.editPassword');
-        Route::post('profile/update-password', [ProfileController::class, 'updatePassword'])->name('profile.updatePassword');
-        Route::resource('relatives', RelativeController::class);
+        Route::get('guest/register', [GuestController::class, 'register'])->name('guest.register');
+        Route::post('guest/register', [GuestController::class, 'store'])->name('guest.store');
 
 
 
-        // admin
-        Route::prefix('admin')->group(function(){
 
-            Route::get('dashboard', [AdminHomeController::class, 'index'])->name('dashboard.index');
-            Route::resource('users', UserController::class);
-            Route::resource('banners', BannerController::class);
-            Route::resource('images', ImageController::class);
-            Route::resource('contacts', ContactController::class);
-            Route::resource('settings', SettingController::class);
-            Route::resource('admin-relatives', AdminRelativeController::class);
-            Route::resource('relative-types', RelativeTypeController::class);
-            Route::resource('roles', RoleController::class);
-            
-            Route::get('images-active\{id}', [ImageController::class, 'active'])->name('images.active');
-            Route::get('images-deactive\{id}', [ImageController::class, 'deactive'])->name('images.deactive');
-            Route::get('page/about-us', [AdminTextPageController::class, 'aboutIndex'])->name('about-us.index');
-            Route::post('page/about-us', [AdminTextPageController::class, 'aboutStore'])->name('about-us.store');
-            Route::get('page/terms', [AdminTextPageController::class, 'termsIndex'])->name('page.terms.index');
-            Route::post('page/terms', [AdminTextPageController::class, 'termsStore'])->name('page.terms.store');
-            Route::get('profile', [AdminProfileController::class, 'index'])->name('admin-profile.index');
-            Route::put('profile', [AdminProfileController::class, 'update'])->name('admin-profile.update');
-            Route::get('orders', [AdminOrderController::class, 'index'])->name('orders.index');
-            Route::delete('orders/{id}', [AdminOrderController::class, 'destroy'])->name('orders.destroy');
-            Route::put('orders/{id}', [AdminOrderController::class, 'update'])->name('orders.update');
+        Route::middleware(['auth', 'verified'])->group(function () {
+
+            Route::get('profile', [ProfileController::class, 'index'])->name('profile.index');
+            Route::post('profile/update', [ProfileController::class, 'update'])->name('profile.update');
+            Route::get('profile/edit-password', [ProfileController::class, 'editPassword'])->name('profile.editPassword');
+            Route::post('profile/update-password', [ProfileController::class, 'updatePassword'])->name('profile.updatePassword');
+            Route::resource('relatives', RelativeController::class);
+
+
+
+            // admin
+            Route::prefix('admin')->group(function () {
+
+                Route::get('dashboard', [AdminHomeController::class, 'index'])->name('dashboard.index');
+                Route::resource('users', UserController::class);
+                Route::resource('banners', BannerController::class);
+                Route::resource('images', ImageController::class);
+                Route::resource('contacts', ContactController::class);
+                Route::resource('settings', SettingController::class);
+                Route::resource('admin-relatives', AdminRelativeController::class);
+                Route::resource('relative-types', RelativeTypeController::class);
+                Route::resource('roles', RoleController::class);
+
+                Route::get('images-active\{id}', [ImageController::class, 'active'])->name('images.active');
+                Route::get('images-deactive\{id}', [ImageController::class, 'deactive'])->name('images.deactive');
+                Route::get('page/about-us', [AdminTextPageController::class, 'aboutIndex'])->name('about-us.index');
+                Route::post('page/about-us', [AdminTextPageController::class, 'aboutStore'])->name('about-us.store');
+                Route::get('page/terms', [AdminTextPageController::class, 'termsIndex'])->name('page.terms.index');
+                Route::post('page/terms', [AdminTextPageController::class, 'termsStore'])->name('page.terms.store');
+                Route::get('profile', [AdminProfileController::class, 'index'])->name('admin-profile.index');
+                Route::put('profile', [AdminProfileController::class, 'update'])->name('admin-profile.update');
+                Route::get('orders', [AdminOrderController::class, 'index'])->name('orders.index');
+                Route::delete('orders/{id}', [AdminOrderController::class, 'destroy'])->name('orders.destroy');
+                Route::put('orders/{id}', [AdminOrderController::class, 'update'])->name('orders.update');
+            });
         });
-    });
 
 
-    Route::get("about-us", [TextPageController::class, 'aboutUs'])->name('about_us.index');
-    Route::get("terms-and-condition", [TextPageController::class, 'terms'])->name('terms.index');
-
-
-
-    Route::get("contact-us", [ContactUsController::class, 'index'])->name('countact_us.index');
-    Route::post("contact-us", [ContactUsController::class, 'store'])->name('countact_us.store');
-
-    Route::get('my-orders', [OrderController::class, 'index'])->name('order.index');
-    Route::post('order/store', [OrderController::class, 'store'])->name('order.store');
+        Route::get("about-us", [TextPageController::class, 'aboutUs'])->name('about_us.index');
+        Route::get("terms-and-condition", [TextPageController::class, 'terms'])->name('terms.index');
 
 
 
+        Route::get("contact-us", [ContactUsController::class, 'index'])->name('countact_us.index');
+        Route::post("contact-us", [ContactUsController::class, 'store'])->name('countact_us.store');
 
-
-    
-
-
-
-
-    
-    
-});
-
-
-
-
-
-
-
+        Route::get('my-orders', [OrderController::class, 'index'])->name('order.index');
+        Route::post('order/store', [OrderController::class, 'store'])->name('order.store');
+    }
+);
